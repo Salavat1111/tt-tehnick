df000b113644faef7e83f3d0382747853be75473
salavat1111 <163salik@rambler.ru>
2021-12-27 17:06:13 +0400

    обновление 27.12.21

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import { FaBorderAll } from "react-icons/fa"; // заказы
 import { FaLayerGroup } from "react-icons/fa"; // история
 import { FaRegSun } from "react-icons/fa"; // настройки
 
-import { HistoryMap, Repair, Settings, СardOrder, Logo, NavigationBottom, BtnBurger } from './cards'
+import { PasswordForma, FormatNamber, Registration, HistoryMap, Repair, Settings, СardOrder, Logo, NavigationBottom, BtnBurger, HomeRepair, } from './cards'
 import { ProfileContact, ProfileEmail, ProfileAdress, ProfileName } from './cards/component-settings';
 import { Routes, Route } from 'react-router-dom';
+import React from 'react';
 
 
+const stepsComponents = {
+  0: HomeRepair,
+  1: Registration,
+  2: FormatNamber,
+  3: PasswordForma,
+};
 
+export const MainContext = React.createContext({});
 
 function App() {
-
 
   const items = [
     { value: 'создать', href: './1', icon: <FaPlusCircle />, },
@@ -21,6 +28,19 @@ function App() {
     { value: 'история', href: './3', icon: <FaLayerGroup />, },
     { value: 'настройки', href: './4', icon: <FaRegSun />, },
   ];
+
+  // const link = ['Мясные', 'Лесные', 'Саленные', 'Маленые', 'Крутые', 'Малые', 'Слепые', 'Топлые',]
+  const [visibleRepir, setVisibleRepair] = React.useState(false);
+  const repair = visibleRepir
+
+
+  const onNextStep = () => {
+    setStep((prev) => prev + 1)
+  }
+
+  const [step, setStep] = React.useState(1)
+  const Step = stepsComponents[step];
+
 
   return (
     <>
@@ -34,6 +54,7 @@ function App() {
           <BtnBurger />
           <div className="content__repair">
             <Routes>
+              {/* <Route path="/" element={<HomeRepair />} /> */}
               <Route path="/1" element={<Repair />} />
               <Route path="/2" element={<СardOrder />} />
               <Route path="/3" element={<HistoryMap />} />
@@ -41,11 +62,20 @@ function App() {
               <Route path="/2-2" element={< ProfileName />} />
               <Route path="/3-3" element={< ProfileContact />} />
               <Route path="/4-4" element={< ProfileEmail />} />
+              {/* <Route path="/5-5" element={< ProfileAdress link={link} />} /> */}
               <Route path="/5-5" element={< ProfileAdress />} />
             </Routes>
           </div>
         </div>
-        <NavigationBottom items={items} />
+
+        {
+          // <Step />
+          // {repair === <HomeRepair /> ? <NavigationBottom items={items} /> : ''}
+        }
+        <MainContext.Provider value={{ step, onNextStep }}>
+          <Step />
+        </MainContext.Provider>
+
       </div>
     </>
   );
