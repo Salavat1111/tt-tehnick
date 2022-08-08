b839e9839dfdccbcbbc3c6fc55a9430985944aa7
salavat1111 <163salik@rambler.ru>
2022-08-08 22:08:24 +0400

    Доработка интерфейса

---
+++ src/cards/about-us/AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './us.css';
+import '../../macro.css';
+
+function AboutUs({ }) {
+
+
+
+    return (
+        <div className="about-us__homepage">
+            <div className="about-us__container"></div>
+        </div >
+    );
+}
+
+
+export default AboutUs;
