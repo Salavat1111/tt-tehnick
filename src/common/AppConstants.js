0b282e777d98404ae1505249c83f86b369708808
Степан Берендяев <sberendyaev@phoenixit.ru>
2022-08-16 08:57:21 +0400

    merge with salavat branch

--- src/common/AppConstants.js
+++ src/common/AppConstants.js
@@ -1,4 +1,4 @@
-export const serverUrl = 'http://localhost:8050';
+export const serverUrl = 'https://ttappstbe.herokuapp.com';
 export const userSettingsUrl = '/rooms/4';
 export const createOrderUrl = '/rooms/1';
 export const loginUrl = '/l';
